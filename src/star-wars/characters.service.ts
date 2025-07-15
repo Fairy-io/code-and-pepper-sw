21d22f6dcdeb70ac1ad850b39424421d2b70b03c
massfice <adrian.marian.tomasz.larysz@gmail.com>
2025-07-15 23:09:28 +0200

    using firestore to retrieve data

--- src/star-wars/characters.service.ts
+++ src/star-wars/characters.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { Character } from './models/Character.model';
+import { Firestore } from '@google-cloud/firestore';
+import { CollectionsProvider } from '../common/collections.provider';
+import { plainToInstance } from 'class-transformer';
+import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class CharactersService {
+    constructor(
+        private readonly collections: CollectionsProvider,
+    ) {}
+
     async getCharacters({
         offset,
         limit,
@@ -13,6 +21,36 @@ export class CharactersService {
         entries: Character[];
         count: number;
     }> {
-        throw new Error('Not implemented');
+        const collection =
+            this.collections.getCharactersCollection();
+
+        const { docs } = await collection
+            .limit(limit)
+            .offset(offset)
+            .get();
+
+        const entries = await Promise.all(
+            docs.map(async (doc) => {
+                const data = doc.data();
+
+                const character = plainToInstance(
+                    Character,
+                    { id: doc.id, ...data },
+                );
+
+                await validateOrReject(character);
+
+                return character;
+            }),
+        );
+
+        const { count } = (
+            await collection.count().get()
+        ).data();
+
+        return {
+            entries,
+            count,
+        };
     }
 }
--- src/star-wars/starWars.module.ts
+++ src/star-wars/starWars.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CharactersResolver } from './characters.resolver';
 import { CharactersService } from './characters.service';
+import { CommonModule } from '../common/common.module';
 
 @Module({
+    imports: [CommonModule],
     providers: [CharactersResolver, CharactersService],
 })
 export class StarWarsModule {}

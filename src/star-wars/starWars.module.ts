5829651491992361844e32c8c20b07a8446ecaf3
massfice <adrian.marian.tomasz.larysz@gmail.com>
2025-07-16 02:13:09 +0200

    updating schema

--- src/star-wars/starWars.module.ts
+++ src/star-wars/starWars.module.ts
@@ -3,11 +3,13 @@ import { CharactersResolver } from './characters.resolver';
 import { CharactersService } from './characters.service';
 import { CommonModule } from '../common/common.module';
 import { CharacterRepository } from './repositories/character.repository';
+import { CreateCharacterResolver } from './createCharacter.resolver';
 
 @Module({
     imports: [CommonModule],
     providers: [
         CharactersResolver,
+        CreateCharacterResolver,
         CharactersService,
         CharacterRepository,
     ],
